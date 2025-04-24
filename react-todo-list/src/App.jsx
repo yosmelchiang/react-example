5cfd9c5f16c2e90a559a346af8a09cad683fc930
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-04-24 15:19:05 +0200

    add motivational text and no more todos text

--- react-todo-list/src/App.jsx
+++ react-todo-list/src/App.jsx
@@ -1,9 +1,11 @@
 import Liste from './components/Liste.jsx'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
   
   const [listElements, setList] = useState(["ting", "andre ting", "oppvask", "mate katten"])
+
+  const [motivationText, setMotivationText] = useState('')
   
   function onElementClick(index) {
     const modifiedArr = [...listElements] // Create a shallow copy of the original array
@@ -11,8 +13,22 @@ function App() {
     setList(modifiedArr) // Update the listElements array
   }
 
+  useEffect(() => {
+    let motivationText;
+
+    if(listElements.length === 0) {
+      motivationText = 'No more work for today 🎉';
+    } else {
+      const sentences = ['Anything interesting going on? 😎', 'Keep it going 👍', 'Got something fun to do? 🤡', 'Whats on your mind? 😊'];
+      motivationText = sentences[Math.floor(Math.random() * sentences.length)];
+    }
+
+    setMotivationText(motivationText)
+  }, [listElements])
+
   return (
-    <div>
+    <div className="listContainer">
+      <h1>{motivationText}</h1>
       <Liste elementer={listElements} handleElementClickAtIndex={onElementClick}/>
     </div>
   )

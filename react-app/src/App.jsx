a7eba2b49e42a1cc1fc1f3a0094e9b69fb4fdbdd
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-04-24 13:23:08 +0200

    add useEffect example

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import "./styles/App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: null
+function App() {
+  const [myArr, setMyArr] = useState([1, 2, 3, 4, 5]);
+  const [isEmptyArr, setisEmptyArr] = useState(false) // Storing the state of our array to check wether its empty or not
+
+  const removeFirstNum = () => {
+    const newArr = [...myArr];
+    newArr.splice(0, 1);
+    setMyArr(newArr);
+  } 
+
+  useEffect(() => {
+    if(myArr.length === 0) { // This will only trigger if myArr.length is 0, meaning its empty
+      setisEmptyArr(true)
     }
-  }
+  }, [myArr]) // We want useEffect to run only when changes to myArr has been made, that way we can check its length everytime its modified
 
-componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(data => this.setState({ data }))
-      .catch(err => console.error(err))
-}
-
-render() {
-  console.log(this.state.data) // Logs the fetched data to the console
-  return(
+  return (
     <div>
-      <pre>{JSON.stringify(this.state.data, null, 2)}</pre> {/* Shows the data as stringified JSON*/}
+      <h1>{isEmptyArr ? 'Array is empty':'Array is not empty'}</h1> {/* Shows a text representing the true / falsy value of isEmptyArr */}
+      <h1>{myArr}</h1>
+      <button onClick={removeFirstNum}>Remove first num state</button>
     </div>
   )
 }
-}
 
-export default App;
+export default App

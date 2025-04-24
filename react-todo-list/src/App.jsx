a5d37e2284f48cae7929625e436e25bab9c0b4bd
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-04-24 07:30:58 +0200

    chore: initiate react todo list project

---
+++ react-todo-list/src/App.jsx
@@ -0,0 +1,15 @@
+function ListeElement({element = ""}) {
+  return <li>{element}</li>
+}
+
+function Liste({elementer = []}) {
+
+}
+
+function App() {
+  return (
+    <h1>Hello world</h1>
+  )
+}
+
+export default App

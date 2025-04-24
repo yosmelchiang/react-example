a19d817e5b7566f435a1f348372afe0532c8b12b
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-04-24 14:51:15 +0200

    add removing of list items

--- react-todo-list/src/App.jsx
+++ react-todo-list/src/App.jsx
@@ -1,13 +1,19 @@
 import Liste from './components/Liste.jsx'
+import { useState } from 'react'
 
 function App() {
   
-  // Dummy array with todo list items
-  const innhold = ["ting", "andre ting", "oppvask", "mate katten"]
+  const [listElements, setList] = useState(["ting", "andre ting", "oppvask", "mate katten"])
   
+  function onElementClick(index) {
+    const modifiedArr = [...listElements] // Create a shallow copy of the original array
+    modifiedArr.splice(index, 1) // Remove 1 item starting from index position
+    setList(modifiedArr) // Update the listElements array
+  }
+
   return (
     <div>
-      <Liste elementer={innhold}/>
+      <Liste elementer={listElements} handleElementClickAtIndex={onElementClick}/>
     </div>
   )
 }
--- react-todo-list/src/components/Liste.jsx
+++ react-todo-list/src/components/Liste.jsx
@@ -1,19 +1,39 @@
 import './Liste.css'
 
 // Creates a <li> element for the string that is passed to this component
-function ListeElement({element = ""}) {
-  return <li>{element}</li>
+function ListeElement({ onClickHandler, element = ""}) { // Receives a function that returns the function that removes an item based on index with the index bound to it
+  
+  // Function that we will be calling when a li item is clicked on
+  function handleClick() {
+    onClickHandler();
+  }
+  
+  return <li onClick={handleClick}>{element}</li> 
 } 
 
 // Processes an array of strings and returns an unordered list with multiple list items for each array string
-function Liste({elementer = []}) {
+function Liste({ handleElementClickAtIndex, elementer = []}) {
   return (
     <ul>
       {
-        elementer.map((e, i) => <ListeElement key={i} element={e}/>) // Iterates through the array and generates a <li> item
+        elementer.map((e, i) => < ListeElement  // Iterates through the array and generates a <li> item
+          key={i} // We have to give this component a key, its for react stuff
+          element={e} // Passing each item of our listElements array to generate a <li> item out of it
+          
+          // Closure occurs here: We are creating an anonymous function that will call handleElementClickAtIndex() once invoked, 
+          // and we are also passing the index value i to it, it will remember i even though we have closed this lexical environment
+          onClickHandler={ function() { handleElementClickAtIndex(i)} }
+        />) 
       }
     </ul>
   )
 }
+
+// Closure
+/**
+ * In JavaScript, a closure is a function that "remembers" its lexical environment, 
+ * meaning it has access to the variables that were in scope when the function was created, 
+ * even after that scope has finished executing.
+ */
 
 export default Liste
